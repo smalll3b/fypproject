761f02664b2a758ddacdeec814ab183aadb3a182
smalll3b <18568asd@gmail.com>
2024-10-17 22:08:13 +0800

    save

--- app/src/main/java/com/example/fypproject/AddProductActivity.java
+++ app/src/main/java/com/example/fypproject/AddProductActivity.java
@@ -1,7 +1,10 @@
 package com.example.fypproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AddProductActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class AddProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.insert_product_staff);
+
+        // 查看商品按钮
+        Button btnViewProduct = findViewById(R.id.btnViewProduct);
+        btnViewProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddProductActivity.this, ProductDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
